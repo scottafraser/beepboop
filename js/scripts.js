ea8c011d2f2a5520220a01b7416639c9156a37a7
Scott Fraser <scottafraser@yahoo.com>
2018-06-08 13:52:46 -0700

    refractor code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-function splitInput (number) {
-  var inArray = number.toString()
-  var stringArray = inArray.split('')
-  return stringArray
-};
-
 function range(start, end) {
     var answer = [];
     for (let i = start; i <= end; i++) {
@@ -13,46 +7,67 @@ function range(start, end) {
 };
 
 function makeRange (make) {
-  var rangeNum = (parseInt(make.join(''))).range(0,make)
+  var rangeNum = (make.join('')).range(0,make)
   return newRange
 };
+
+function doubleDigits (two) {
+  var inArray = one.toString()
+  var stringArray = inArray.split('')
+    for (x = 0; x < stringArray.length; x ++) {
+      if (stringArray[x] === '1') {
+        digitArray.push('Boop!')
+        break
+      } else if (stringArray[x] === '0' ) {
+        digitArray.push('Beep!')
+        break
+     }
+  }
+}
+
+function singleDigits(one) {
+  var digitArray = []
+  for (x = 1; x < one.length; x ++) {
+    // if (one[x] !== 0 || one[x] !== 1 && inArray < 10 ) {
+    //     digitArray.push(one[x])
+       // if (one > 9 ) {
+       //  doubleDigits(one)
+        if (one[x] === 1) {
+        digitArray.push('Boop!')
+      } else if (one[x] === 0 ) {
+        digitArray.push('Beep!')
+      } else if (one[x] !== 0 || one[x] !== 1)
+        digitArray.push(one[x])
+   }
+
+  return digitArray
+  console.log(digitArray);
+}
+
 
 $(document).ready(function() {
   $("form#puzzle").submit(function(event){
       event.preventDefault();
-      var number = splitInput($('#inputNumber').val());
-      var realNumber = $('#inputNumber').val();
-      var boopArray = []
+      // var number = splitInput($('#inputNumber').val());
+      var number = $('#inputNumber').val();
+      var theArray = []
       var array = []
 
       for (x = 0; x < number.length; x ++) {
-        if (parseInt(number.join('')) % 3 === 0 && (number[x]) !== '0') {
+        if (number % 3 === 0 && (number[x]) !== '0') {
            array.push("I'm sorry, Dave.")
           break
-        } else if (number[x] !== '0' || number[x] !== '0' ) {
-          boopArray.push(range(0,realNumber))
-          for (i = 0; i < boopArray.length; i ++) {
-            if (boopArray[x] === 1) {
-              boopArray.push('beep')
-            } else if (boopArray[x] === 0 ) {
-              boopArray.push('boop')
-            }
-           console.log(boopArray);
-          };
+        } else {
+          var boopArray = (range(0, number))
+          console.log(boopArray);
+          theArray = singleDigits(boopArray)
+          array.push(theArray)
 
-        // } else if (number[x] === "1") {
-        //   array.push('beep')
-        // } else if (number[x] === '0' ) {
-        //   array.push('boop')
-        //   break
-
-        } else {}
+        }
        };
 
       var finalanswer = array
 
       $('#result').text(finalanswer);
-
-
+    });
   });
-});
